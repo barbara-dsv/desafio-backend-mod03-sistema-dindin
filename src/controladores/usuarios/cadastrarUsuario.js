c542954f51caccccfd36a3d8ae13bcb6a3d537d5
cybergrazi <graziellecvergilio@gmail.com>
2023-11-20 20:51:16 -0300

    Criado código base para cadastrar usuaário

---
+++ src/controladores/usuarios/cadastrarUsuario.js
@@ -0,0 +1,15 @@
+const pool = require('../usuarios/controladores/conexao.js') // verificar se está certa essa parte de importar -Gra
+
+const cadastrarUsuario = async (req, res) => {
+    const { nome, email, senha } = req.body
+
+    try {
+        const novoUsuario = await pool.query('insert into usuarios (nome, email, senha) values ($1, $2, $3) returning *', [nome, email, senha])
+
+        return res.status(201).json(novoUsuario.rows[novoUsuario.lenght - 1])
+    } catch (error) {
+        return res.status(500).json({ mensagem: 'Erro do servidor' })
+    }
+}
+
+module.exports = { cadastrarUsuario }
