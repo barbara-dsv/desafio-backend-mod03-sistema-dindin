2fe95dff5b3ea78d7861f2ffbc0085d3f1956708
Bárbara Lourenço Tavares <babisolua@gmail.com>
2023-11-21 11:19:34 -0300

    rota de login e intermediário implementados

--- src/rotas.js
+++ src/rotas.js
@@ -2,11 +2,18 @@ const express = require('express')
 
 // Controladores
 const cadastrarUsuario = require('./controladores/usuarios/cadastrarUsuario')
+const login = require('./controladores/usuarios/login')
+//importação do intermadiário
+const verificarUsuario = require('./intermediário/autenticacao')
 
 const rotas = express()
 
-//Endpoints
+//Endpoints do usuário
 rotas.post('/usuario', cadastrarUsuario)
+rotas.post('/login', login)
+
+//intermediário
+rotas.use(verificarUsuario)
 
 
 module.exports = rotas
