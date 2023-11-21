8d2cece8828c13d689c26c2d70519f42aaecf306
cybergrazi <graziellecvergilio@gmail.com>
2023-11-21 15:32:25 -0300

    Organização rotas

--- src/rotas.js
+++ src/rotas.js
@@ -1,22 +1,22 @@
 const express = require('express')
+const verificarUsuario = require('./intermediadores/autenticacao') //importação do intermadiário autenticação
+
+const rotas = express()
 
 // Controladores
 const cadastrarUsuario = require('./controladores/usuarios/cadastrarUsuario')
 const login = require('./controladores/usuarios/login')
 const detalharPerfilUsuario = require('./controladores/usuarios/detalharPerfilUsuario')
-//importação do intermadiário
-const verificarUsuario = require('./intermediário/autenticacao')
 
-
-const rotas = express()
-
-//Endpoints do usuário
+//Endpoint cadastrar usuário
 rotas.post('/usuario', cadastrarUsuario)
-rotas.post('/login', login)
-rotas.get('/usuario', detalharPerfilUsuario)
 
-//intermediário
+//intermediário (daqui pra baixo, todos usam a verificação)
 rotas.use(verificarUsuario)
 
+//Endpoints do usuário
+rotas.post('/login', login)
+rotas.get('/usuario', detalharPerfilUsuario)
+rotas.put('/usuario',)
 
 module.exports = rotas
