34443710859bbb9a2374be8b84141c14567b5851
Bárbara Lourenço Tavares <babisolua@gmail.com>
2023-11-21 13:16:07 -0300

    rota de detalhar perfil do usuario logado

--- src/rotas.js
+++ src/rotas.js
@@ -3,14 +3,17 @@ const express = require('express')
 // Controladores
 const cadastrarUsuario = require('./controladores/usuarios/cadastrarUsuario')
 const login = require('./controladores/usuarios/login')
+const detalharPerfilUsuario = require('./controladores/usuarios/detalharPerfilUsuario')
 //importação do intermadiário
 const verificarUsuario = require('./intermediário/autenticacao')
+
 
 const rotas = express()
 
 //Endpoints do usuário
 rotas.post('/usuario', cadastrarUsuario)
 rotas.post('/login', login)
+rotas.get('/usuario', detalharPerfilUsuario)
 
 //intermediário
 rotas.use(verificarUsuario)
