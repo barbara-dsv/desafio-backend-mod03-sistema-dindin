78479b93933bf0d75dd76dce413818d378fd1711
Bárbara Lourenço Tavares <babisolua@gmail.com>
2023-11-21 20:39:01 -0300

    rota de listar categoria implementada

--- src/rotas.js
+++ src/rotas.js
@@ -7,16 +7,19 @@ const rotas = express()
 const cadastrarUsuario = require('./controladores/usuarios/cadastrarUsuario')
 const login = require('./controladores/usuarios/login')
 const detalharPerfilUsuario = require('./controladores/usuarios/detalharPerfilUsuario')
+const listarCategorias = require('./controladores/categorias/listarCategorias')
 
 //Endpoint cadastrar usuário
 rotas.post('/usuario', cadastrarUsuario)
+rotas.post('/login', login) //o login não precisa de verificação, ele que vai gerar o token para autenticar o resto dos endpoints
 
 //intermediário (daqui pra baixo, todos usam a verificação)
 rotas.use(verificarUsuario)
 
 //Endpoints do usuário
-rotas.post('/login', login)
 rotas.get('/usuario', detalharPerfilUsuario)
 rotas.put('/usuario',)
+//Endpoint de categoria
+rotas.get('/categoria', listarCategorias)
 
 module.exports = rotas
