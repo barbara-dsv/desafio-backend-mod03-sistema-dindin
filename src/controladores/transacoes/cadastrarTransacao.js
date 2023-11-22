eff28b9f5acab00662a728c3ee129577f98f6d35
cybergrazi <graziellecvergilio@gmail.com>
2023-11-22 13:00:16 -0300

    Verificação de categoria_id

--- src/controladores/transacoes/cadastrarTransacao.js
+++ src/controladores/transacoes/cadastrarTransacao.js
@@ -3,6 +3,14 @@ const pool = require('../../conexao')
 const cadastrarTransacao = async (req, res) => {
     const { descricao, valor, categoria_id, tipo } = req.body
 
+    if (!descricao || !valor || !categoria_id || !tipo) {
+        return res.status(400).json({ mensagem: 'Todos os campos são obrigatórios' })
+    }
+
+    if (categoria_id < 1) {
+        return res.status(400).json({ mensagem: 'Categoria não encontrada' })
+    }
+
     try {
         const novaTransacao = await pool.query(
             'insert into transacoes (descricao, valor, categoria_id, tipo) values ($1, $2, $3, $4)'[
