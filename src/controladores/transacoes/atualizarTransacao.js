66b1ccf6070f10fbfcc2b74fec5ea2f7a9be6efb
cybergrazi <graziellecvergilio@gmail.com>
2023-11-22 14:18:34 -0300

    atualizarTransicao comecei

---
+++ src/controladores/transacoes/atualizarTransacao.js
@@ -0,0 +1,11 @@
+const pool = require('../../conexao')
+
+const atualizarTransacao = async (req, res) => {
+    const { descricao, valor, data, categoria_id, tipo } = req.body
+    const { id } = req.params
+
+    try {
+        const // continuar
+    }
+
+ }
