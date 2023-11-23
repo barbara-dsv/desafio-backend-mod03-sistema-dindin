088c7cb4bee3622f7dc86522e55fed165f3e9da4
Bárbara Lourenço Tavares <babisolua@gmail.com>
2023-11-23 08:07:04 -0300

    validações implementadas e codigo refatorado

--- src/controladores/transacoes/cadastrarTransacao.js
+++ src/controladores/transacoes/cadastrarTransacao.js
@@ -2,22 +2,23 @@ const pool = require('../../conexao')
 
 const cadastrarTransacao = async (req, res) => {
     const { descricao, valor, categoria_id, tipo } = req.body
+    const { id } = req.usuario
 
     if (!descricao || !valor || !categoria_id || !tipo) {
-        return res.status(400).json({ mensagem: 'Todos os campos são obrigatórios' })
+        return res.status(400).json({ mensagem: 'Todos os campos obrigatórios devem ser informados.' })
     }
-
-    if (categoria_id < 1) {
-        return res.status(400).json({ mensagem: 'Categoria não encontrada' })
-    }
-
+    if (tipo != "entrada" && tipo != "saida") return res.status(400).json({ message: "O campo 'tipo' deve ser 'entrada' ou 'saida'." })
     try {
-        const novaTransacao = await pool.query(
-            'insert into transacoes (descricao, valor, categoria_id, tipo) values ($1, $2, $3, $4)'[
-            descricao, valor, categoria_id, tipo])
+        const { rows: categoriaExistente } = await pool.query('select * from categorias where id = $1', [categoria_id])
+        if (categoriaExistente === 0) return res.status(400).json({ mensagem: 'Categoria não encontrada.' })
 
-        return res.status(204).send()
-    } catch {
+        const query = 'insert into transacoes (descricao, valor, categoria_id, usuario_id, tipo) values ($1, $2, $3, $4, $5)returning *'
+        const { rows } = await pool.query(query, [descricao, valor, categoria_id, id, tipo])
+        console.log(rows[0])
+
+        return res.status(201).json(rows)
+    } catch (error) {
+        console.log(error.message)
         return res.status(500).json({ mensagem: 'Erro do servidor' })
     }
 }
