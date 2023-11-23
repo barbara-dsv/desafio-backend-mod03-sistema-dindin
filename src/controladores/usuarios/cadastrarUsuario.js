62e21b739bb1db644152ed457a0b56f2fded499a
Bárbara Lourenço Tavares <babisolua@gmail.com>
2023-11-22 22:28:29 -0300

    endpoint 'cadastrarUsuario' corrigido/implementado

--- src/controladores/usuarios/cadastrarUsuario.js
+++ src/controladores/usuarios/cadastrarUsuario.js
@@ -1,14 +1,23 @@
 const pool = require('../../conexao')
-// verificar se está certa essa parte de importar -Gra
-
+const bcrypt = require('bcrypt')
 
 const cadastrarUsuario = async (req, res) => {
     const { nome, email, senha } = req.body
+    if (!nome || !email || !senha) return res.status(400).json({ menssage: "Todos os campos são obrigatórios" })
     try {
-        const novoUsuario = await pool.query('insert into usuarios (nome, email, senha) values ($1, $2, $3) returning *', [nome, email, senha])
+        const { rowCount } = await pool.query('select * from usuarios where email = $1', [email])
 
-        return res.status(201).json(novoUsuario.rows[novoUsuario.lenght - 1])
+        if (rowCount > 0) return res.status(400).json({ message: "Já existe usuário cadastrado com o e-mail informado." })
+
+        const senhaCriptografada = await bcrypt.hash(senha, 10)
+
+        const { rows } = await pool.query('insert into usuarios (nome, email, senha) values ($1, $2, $3) returning *', [nome, email, senhaCriptografada])
+
+        const { senha: _, ...usuarioCadastrado } = rows[0]
+
+        return res.status(201).json(usuarioCadastrado)
     } catch (error) {
+        console.log(error.message)
         return res.status(500).json({ mensagem: 'Erro do servidor' })
     }
 }
