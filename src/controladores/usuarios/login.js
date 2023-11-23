1caeb5a5ff3ac5b054955dc77550287b3bcbcd9c
Bárbara Lourenço Tavares <babisolua@gmail.com>
2023-11-22 22:29:08 -0300

    senha criptograda e validação de email

--- src/controladores/usuarios/login.js
+++ src/controladores/usuarios/login.js
@@ -1,20 +1,26 @@
 const pool = require('../../conexao')
 const jwt = require('jsonwebtoken')
 const senhaJwt = require('../../senha.jwt')
+const bcrypt = require('bcrypt')
+
 const login = async (req, res) => {
     const { email, senha } = req.body
     //validação de campos obrigatórios 
-    if (!email || !senha) return res.status(400).json({ message: "Todos os campos são obrigatórios" })
+    if (!email || !senha) return res.status(400).json({ message: "Todos os campos são obrigatórios." })
     try {
         //validar e verificar email
         const usuario = await pool.query('select * from usuarios where email = $1', [email])
 
         if (usuario.rowCount < 1) {
-            return res.status(404).json({ mensagem: 'email ou senha inválida' })
+            return res.status(404).json({ mensagem: 'Usuário e/ou senha inválido(s).' })
         }
 
         //verificar se a senha é válida 
+        const senhaValida = await bcrypt.compare(senha, usuario.rows[0].senha)
 
+        if (!senhaValida) {
+            return res.status(400).json({ mensagem: 'Usuário e/ou senha inválido(s).' })
+        }
         //gerar token de autenticação 
         const token = jwt.sign({ id: usuario.rows[0].id }, senhaJwt, { expiresIn: '2h' })
 
@@ -22,7 +28,6 @@ const login = async (req, res) => {
 
         return res.json({ usuario: usuarioLogado, token })
     } catch (error) {
-        console.log(error.message)
         return res.status(500).json({ mensagem: 'Erro do servidor' })
     }
 
