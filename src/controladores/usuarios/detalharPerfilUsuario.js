4311a28f64292fa314c88f3edf0427da9505ede7
Bárbara Lourenço Tavares <babisolua@gmail.com>
2023-11-22 22:31:54 -0300

    mensagem json alterada

--- src/controladores/usuarios/detalharPerfilUsuario.js
+++ src/controladores/usuarios/detalharPerfilUsuario.js
@@ -6,7 +6,7 @@ const detalharPerfilUsuario = async (req, res) => {
     const { authorization } = req.headers;
 
     if (!authorization) {
-        return res.status(401).json({ mensagem: 'Não autorizado' })
+        return res.status(401).json({ mensagem: 'Para acessar este recurso um token de autenticação válido deve ser enviado.' })
     }
     const token = authorization.split(' ')[1]
     try {
