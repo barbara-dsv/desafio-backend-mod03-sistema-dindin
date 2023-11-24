b224a1f8da207134f839b17cb9056f6bf6ab6bac
Bárbara Lourenço Tavares <babisolua@gmail.com>
2023-11-24 11:51:57 -0300

    retirado comentários

--- src/controladores/usuarios/detalharPerfilUsuario.js
+++ src/controladores/usuarios/detalharPerfilUsuario.js
@@ -1,17 +1,16 @@
-const jwt = require('jsonwebtoken')
-const senhaJwt = require('../../senha.jwt')
 const pool = require('../../conexao')
 
 const detalharPerfilUsuario = async (req, res) => {
     const { id } = req.usuario;
+
     try {
-        // obter o usuário com base no id
+
         const { rows, rowCount } = await pool.query('select * from usuarios where id = $1', [id])
-        //verifica se usuario foi encontrado
-        if (rowCount < 1) return res.status(401).json({ mensagem: 'Não autorizado' })
+
+        if (rowCount < 1) return res.status(401).json({ mensagem: 'Não autorizado.' })
         req.usuario = rows[0]
-        //remove a senha do objeto do usuário
-        const { senha: _, ...usuarioLogado } = rows[0]
+
+        const { senha: _, ...usuarioLogado } = req.usuario
 
         return res.status(200).json(usuarioLogado)
 
--- src/controladores/usuarios/login.js
+++ src/controladores/usuarios/login.js
@@ -5,23 +5,23 @@ const bcrypt = require('bcrypt')
 
 const login = async (req, res) => {
     const { email, senha } = req.body
-    //validação de campos obrigatórios 
+
     if (!email || !senha) return res.status(400).json({ message: "Todos os campos são obrigatórios." })
     try {
-        //validar e verificar email
+
         const usuario = await pool.query('select * from usuarios where email = $1', [email])
 
         if (usuario.rowCount < 1) {
             return res.status(404).json({ mensagem: 'Usuário e/ou senha inválido(s).' })
         }
 
-        //verificar se a senha é válida 
+
         const senhaValida = await bcrypt.compare(senha, usuario.rows[0].senha)
 
         if (!senhaValida) {
             return res.status(400).json({ mensagem: 'Usuário e/ou senha inválido(s).' })
         }
-        //gerar token de autenticação 
+
         const token = jwt.sign({ id: usuario.rows[0].id }, senhaJwt, { expiresIn: '2h' })
 
         const { senha: _, ...usuarioLogado } = usuario.rows[0]
